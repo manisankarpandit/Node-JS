1f8687b58fd79f681b79828127c996828eb03a11
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-05-26 11:47:58 +0530

    add

--- Moduless/hello.js
+++ Moduless/hello.js
@@ -1,13 +1,15 @@
 // console.log("Hello Dunia");
 
-const math = require("./math");
+//import
+// const math = require("./math");
 
+// console.log("Math value is ",math.add(2,5));
 
+//or
+//we can import directly function
+const {add,sub} = require("./math");
 
-console.log("Math value is ",math(2,5));
-
-
-
-
+console.log("Math value is ",add(2,5));
+console.log("Math value is ",sub(2,5));
 
 
--- Moduless/math.js
+++ Moduless/math.js
@@ -4,10 +4,16 @@ function add(a,b){
     return a+b;
 }
 
+function sub(a,b){
+    return a-b;
+}
 
 
-module.exports = add;
-
+module.exports = {
+    add,
+    sub
+};
+ 
 
 
 
