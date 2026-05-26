82f82d51c676ee3905a191f372587898f07b9fc1
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-05-26 13:50:09 +0530

    add

--- PG2_server/index.js
+++ PG2_server/index.js
@@ -1,8 +1,22 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req,res)=>{
     console.log("New Req Rec..");
-    res.end("Hello From Server");
+
+    //comming request k liya a log creation using fs module import in upper
+
+    const log = `${Date.now()}: New Req Received..\n`;
+    fs.appendFile('log.txt',log, (err,data)=>{
+        switch(req.url){
+            case '/':res.end("hOMEPAGE");
+            break;
+            case '/about' : res.end("I am Mani");
+            break;
+            default : res.end("404 Not Found");
+        }
+        // res.end("Hello From Server again");
+    });
 });
 //we needed a port number with callback function for run the server
 
