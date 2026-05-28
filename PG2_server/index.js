b9f50f729b132e39714d9e0da81fd0c8fe9609f0
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-05-28 11:33:49 +0530

    commit

--- PG2_server/index.js
+++ PG2_server/index.js
@@ -1,16 +1,20 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req,res)=>{
-    console.log("New Req Rec..");
-
+    // console.log("New Req Rec..");
+    if(req.url==="/favicon.ico") return res.end(); 
     //comming request k liya a log creation using fs module import in upper
 
-    const log = `${Date.now()}: New Req Received..\n`;
+    const log = `${Date.now()}:${req.url} New Req Received..\n`;
+    const myUrl = url.parse(req.url);
+    console.log(myUrl);
+
     fs.appendFile('log.txt',log, (err,data)=>{
         //use always non blogging request
         switch(req.url){
-            case '/':res.end("hOMEPAGE");
+            case '/':res.end("HOMEPAGE");
             break;
             case '/about' : res.end("I am Mani");
             break;
