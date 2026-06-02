b96a1d2c34574f4065e46d0b3eecc792f6052c79
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-06-02 12:08:03 +0530

    add status code

--- Project01/index.js
+++ Project01/index.js
@@ -67,10 +67,17 @@ app.route("/api/users/:id")
 app.post('/api/users',(req,res)=>{
     //TODO : create new user
     const body = req.body; //Stores the submitted data in body.
+
+    //This is about status code -> mdn http responce status code
+    if(!body || !body.first_name || !body.last_name ||!body.email ||!body.gender || !body.job_title){
+        return res.status(400).json({msg:'All fields are required'})
+    }
+
     users.push({...body,id:users.length+1});
     //JSON.stringify(users) converts the JavaScript array into JSON text.
     fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
         return  res.status(201).json({status:"success",id:users.length});
+                 //sucessful ststus code 201
     });
 });
 
