817e890ba0e49d1a5f825fbf67cff5388ff41f13
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-06-08 01:50:47 +0530

    addmongo

--- Project01/index.js
+++ Project01/index.js
@@ -106,16 +106,27 @@ app.post('/api/users',(req,res)=>{
     const body = req.body; //Stores the submitted data in body.
 
     //This is about status code -> mdn http responce status code 
-    if(!body || !body.first_name || !body.last_name ||!body.email ||!body.gender || !body.job_title){
+    if(
+       !body || 
+       !body.first_name ||
+       !body.last_name ||
+       !body.email ||
+       !body.gender || 
+       !body.job_title
+    ){
         return res.status(400).json({msg:'All fields are required'})
     }
 
-    users.push({...body,id:users.length+1});
-    //JSON.stringify(users) converts the JavaScript array into JSON text.
-    fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
-        return  res.status(201).json({status:"success",id:users.length});
-                 //sucessful ststus code 201
-    });
+    // //without use mondoDB
+    // users.push({...body,id:users.length+1});
+    // //JSON.stringify(users) converts the JavaScript array into JSON text.
+    // fs.writeFile("./MOCK_DATA.json",JSON.stringify(users),(err,data)=>{
+    //     return  res.status(201).json({status:"success",id:users.length});
+    //              //sucessful ststus code 201
+    // });
+
+    //using mongodb
+
 });
 
 
