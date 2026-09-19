dabe7ddc54a4fab2107e2ffb2b3dff019499c791
Mani Sankar Pandit <msankarpandit@gmail.com>
2026-09-19 18:56:38 +0530

    add

--- Rest_API/index.js
+++ Rest_API/index.js
@@ -26,6 +26,22 @@ app.get("/api/users/:id",(req,res)=>{
     return res.json(user);
 });
 
+app.post("/api/users",(req,res)=>{
+    //create new user
+    return res.json({status:pending});
+})
+
+app.patch("/api/users/:id",(req,res)=>{
+    //Edit user with id
+    return res.json({status:pending});
+});
+
+app.delete("/api/users/:id",(req,res)=>{
+    //create new user
+    return res.json({status:pending});
+});
+
+
 
 
 
